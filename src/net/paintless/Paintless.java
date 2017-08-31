d926ef5e1ea0c593f253f4ff42aaf6f10fc08a1a
LapisSea <toni.tube.you@gmail.com>
2017-08-31 18:52:35 +0200

    a

--- src/net/paintless/Paintless.java
+++ src/net/paintless/Paintless.java
@@ -1,4 +1,4 @@
-package net.paintless.launch;
+package net.paintless;
 
 import com.lapissea.util.LogUtil;
 
--- src/net/paintless/program/gui/MainWindow.java
+++ src/net/paintless/program/gui/MainWindow.java
@@ -1,10 +1,13 @@
 package net.paintless.program.gui;
 
+import com.lapissea.util.LogUtil;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import net.paintless.Paintless;
 
 public class MainWindow extends Application{
 	
@@ -27,6 +30,7 @@ public class MainWindow extends Application{
 		Scene scene=new Scene(layout, 600, 400);
 		main.setScene(scene);
 		main.show();
+		LogUtil.println(System.currentTimeMillis()-Paintless.start);
 	}
 	
 }
