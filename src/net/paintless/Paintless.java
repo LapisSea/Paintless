490345e539b69de75c62c541643bff9856de1697
LapisSea <toni.tube.you@gmail.com>
2017-09-01 15:36:50 +0200

    gui implement / general improvements

--- src/net/paintless/Paintless.java
+++ src/net/paintless/Paintless.java
@@ -1,29 +1,74 @@
 package net.paintless;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+
 import com.lapissea.util.LogUtil;
 
-import net.paintless.load.LoadManager;
 import net.paintless.load.LoadRequest;
 
 public class Paintless{
 	
 	public static final Paintless INSTANCE=new Paintless();
+	
 	public static long start=System.currentTimeMillis();
+	
+	public static final ForkJoinPool LOADING_POOL=new ForkJoinPool(1, ForkJoinPool.defaultForkJoinWorkerThreadFactory, null, true);
 	
 	public static void main(String[] args){
 		LogUtil.__.INJECT_DEBUG_PRINT(true);
-		LogUtil.__.INJECT_FILE_LOG("log.txt");
-		System.setProperty("sun.java2d.opengl", "false");
+//		LogUtil.__.INJECT_FILE_LOG(file);
+//		System.setProperty("sun.java2d.opengl", "true");
 //		System.setProperty("sun.java2d.noddraw", "true");
-		System.setProperty("awt.useSystemAAFontSettings", "on");
+//		System.setProperty("awt.useSystemAAFontSettings", "on");
 		INSTANCE.start();
 	}
 	
-	public final LoadManager loadManager=new LoadManager();
+	private static final List<LoadRequest>	loadQ	=new ArrayList<>();
+	private static final Object				LOCK	=new Object();
+	
+	private static void run(){
+		try{
+			while(true){
+				synchronized(LOCK){
+					if(loadQ.isEmpty()){
+						LOCK.wait(5000);
+						continue;
+					}
+					load(loadQ.remove(loadQ.size()-1));
+				}
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	private static void load(LoadRequest request){
+		try{//start
+			Class<?> c=Class.forName(request.classPath);
+			Method start=c.getDeclaredMethod("start");
+			start.setAccessible(true);
+			start.invoke(null);
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}catch(Exception e){}
+	}
+	
+	public static void request(LoadRequest request){
+		synchronized(LOCK){
+			loadQ.add(request);
+			loadQ.sort(null);
+			try{
+				LOCK.notifyAll();
+			}catch(Exception e){}
+		}
+	}
 	
 	public void start(){
-		loadManager.request(new LoadRequest("gui.MainWindow", Integer.MAX_VALUE, false));
-		loadManager.request(new LoadRequest("editing.OpenCL", 0, true));
-		loadManager.start();
+		request(new LoadRequest("gui.MainWindow", Integer.MAX_VALUE, false));
+		request(new LoadRequest("editing.OpenCL", 0, true));
+		run();
 	}
 }
--- src/net/paintless/program/gui/MainWindow.java
+++ src/net/paintless/program/gui/MainWindow.java
@@ -1,66 +1,275 @@
 package net.paintless.program.gui;
 
+import static com.lapissea.util.UtilL.*;
+
+import java.io.InputStream;
+import java.util.function.Consumer;
+
 import com.lapissea.util.LogUtil;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import net.paintless.Paintless;
 
 public class MainWindow extends Application{
 	
-	static void start() throws Exception{
+	static boolean loading=true;
+	
+	private static final Color	winActiveBord	=new Color(0.09375, 0.51171875, 0.83984375, 1);
+	private static final Color	winUnactiveBord	=new Color(0.5, 0.5, 0.5, 0.6640625);
+	
+	public static void start() throws Exception{
 		launch(MainWindow.class);
 	}
 	
-	Stage	window;
-	Scene	mainScene,configScene;
+	public static boolean loading(){
+		return loading;
+	}
+	
+	Stage		window;
+	BorderPane	contentPane;
+	ImageView	unmax	=null;
+	ImageView	max		=null;
+	
+	boolean	dragged,doubleClick;
+	double	xDrag,yDrag;
+	
+	BorderPane	mainGui;
+	StackPane	fileGui;
+	Canvas		drawCanvas;
 	
 	@Override
 	public void start(Stage win) throws Exception{
 		window=win;
-		
 		window.setTitle("Paintless");
+		window.initStyle(StageStyle.TRANSPARENT);
+		initBase();
 		
-		initMainScene();
-		initConfigScene();
+		mainGui=new BorderPane();
 		
-		window.setScene(mainScene);
+		HBox top=cls(new HBox(), "bar");
+		mainGui.setTop(top);
 		
+		top.getChildren().add(btn("File", e->setGui(fileGui), "btn", "w60"));
+		top.getChildren().add(btn("Home", e->setGui(fileGui), "btn", "w60"));
+		top.getChildren().add(btn("Edit", e->setGui(fileGui), "btn", "w60"));
+		top.getChildren().add(btn("Effect", e->setGui(fileGui), "btn", "w60"));
+		top.getChildren().add(btn("Animate", e->setGui(fileGui), "btn", "w60"));
+		
+		ScrollPane scroll=new ScrollPane();
+		mainGui.setCenter(scroll);
+		scroll.getStyleClass().add("scroll-bar");
+		
+		StackPane stack=new StackPane();
+		scroll.setContent(stack);
+		drawCanvas=new Canvas(500,500);
+		stack.getChildren().add(drawCanvas);
+		
+		scroll.setBackground(new Background(new BackgroundFill(new Color(0.8984375, 0.8984375, 0.8984375, 1), new CornerRadii(0), Insets.EMPTY)));
+		
+		//////////////
+		
+		fileGui=new StackPane();
+		
+		setGui(mainGui);
+		
+		new AnimationTimer(){
+			
+			@Override
+			public void handle(long now){
+				updateDraw();
+			}
+		}.start();
 		window.show();
-		
+		loading=false;
 		LogUtil.println(System.currentTimeMillis()-Paintless.start);
 	}
 	
-	private void initMainScene(){
-		Button button=new Button("open config");
-		
-		button.setOnAction(e->{
-			window.setScene(configScene);
-			LogUtil.println(e);
-		});
-		
-		StackPane layout=new StackPane();
-		layout.getChildren().add(button);
-		
-		mainScene=new Scene(layout, 600, 400);
-		
-	}
-	private void initConfigScene(){
-		Button button=new Button("open main");
-		
-		button.setOnAction(e->{
-			window.setScene(mainScene);
-			LogUtil.println(e);
-		});
-		
-		StackPane layout=new StackPane();
-		layout.getChildren().add(button);
-		
-		configScene=new Scene(layout, 600, 400);
-		
+	private void updateDraw(){
+		GraphicsContext ctx=drawCanvas.getGraphicsContext2D();
+		ctx.clearRect(0,0,drawCanvas.getWidth(),drawCanvas.getHeight());
+		ctx.setFill(Color.BLACK);
+		ctx.setStroke(Color.BLACK);
+		ctx.strokeRect(0,0,drawCanvas.getWidth(),drawCanvas.getHeight());
+		ctx.beginPath();
+		ctx.moveTo(0, 0);
+		ctx.lineTo(drawCanvas.getWidth()+(Math.random()-0.5)*50,drawCanvas.getHeight()+(Math.random()-0.5)*50);
+		ctx.stroke();
 	}
 	
+	public void setGui(Node gui){
+		contentPane.setCenter(gui);
+	}
+	
+	private void initBase(){
+		
+		int shadowSize=20;
+		
+		StackPane root=new StackPane();
+		root.setEffect(new DropShadow(shadowSize, new Color(0, 0, 0, 0.5)));
+		root.setPadding(new Insets(shadowSize));
+		root.setBackground(Background.EMPTY);
+		contentPane=new BorderPane();
+		root.getChildren().add(contentPane);
+		contentPane.setBorder(new Border(new BorderStroke(winUnactiveBord, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+		
+		Scene scene=new Scene(root, 600, 400);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		
+		HBox titleBar=cls(new HBox(), "bar");
+		
+		Pane spacer=new Pane();
+		HBox.setHgrow(spacer, Priority.ALWAYS);
+		Label title=new Label(window.getTitle());
+		title.setCache(true);
+		title.setPrefHeight(40);
+		title.setPadding(new Insets(0, 0, 0, 10));
+		spacer.maxHeight(1);
+		
+		titleBar.setOnMousePressed(e->{
+			dragged=false;
+			if(doubleClick=e.getClickCount()==2){
+				window.setMaximized(true);
+				return;
+			}
+			xDrag=e.getScreenX()-window.getX();
+			yDrag=e.getScreenY()-window.getY();
+		});
+		titleBar.setOnMouseDragged(e->{
+			if(window.isFullScreen()||doubleClick) return;
+			if(window.isMaximized()){
+				double percentX=e.getSceneX()/(window.getWidth()-120);
+				yDrag=e.getScreenY()+shadowSize;
+				window.setMaximized(false);
+				xDrag=(window.getWidth()-122+shadowSize)*percentX+shadowSize;
+				return;
+			}
+			window.setX(e.getScreenX()-xDrag);
+			window.setY(e.getScreenY()-yDrag);
+			dragged=true;
+		});
+		titleBar.setOnMouseReleased(e->{
+			if(!dragged) return;
+			if(e.getScreenY()==0){
+				new Thread(()->{
+					sleep(30);
+					Platform.runLater(()->window.setMaximized(true));
+				}).start();
+			}
+		});
+		
+//		new Image(src("min.png")) new Image(src("x.png"))
+		Button minMax,min,x;
+		
+		titleBar.getChildren().addAll(
+				title,
+				spacer,
+				min=btn("", e->window.setIconified(true), "btn", "w40"),
+				minMax=btn("", e->window.setMaximized(!window.isMaximized()), "btn", "w40"),
+				x=btn("", e->{
+					window.hide();
+					System.exit(0);
+				}, "btn", "w40"));
+		
+		loadImg("max.png", i->{
+			max=i;
+			minMax.setGraphic(max);
+		});
+		loadImg("unmax.png", i->{
+			unmax=i;
+			if(!window.isMaximized()) minMax.setGraphic(unmax);
+		});
+		loadImg("min.png", min::setGraphic);
+		loadImg("x.png", x::setGraphic);
+		
+		contentPane.setTop(titleBar);
+		
+		scene.setFill(Color.TRANSPARENT);
+		
+		window.setScene(scene);
+		ResizeHelper.addResizeListener(shadowSize+1, window);
+		
+		window.setMinHeight(42+shadowSize*2);
+		window.setMinWidth(106+shadowSize*2);
+		
+		window.focusedProperty().addListener(e->updateBorder((BorderPane)window.getScene().getRoot().getChildrenUnmodifiable().get(0)));
+		window.maximizedProperty().addListener(e->{
+			StackPane root0=(StackPane)window.getScene().getRoot();
+			if(window.isMaximized()){
+				minMax.setGraphic(unmax);
+				root0.setPadding(new Insets(0));
+			}else{
+				root0.setPadding(new Insets(shadowSize));
+				minMax.setGraphic(max);
+			}
+			updateBorder((BorderPane)root0.getChildrenUnmodifiable().get(0));
+		});
+		window.widthProperty().addListener(e->{
+			if(window.getWidth()<window.getMinWidth()) window.setWidth(window.getMinWidth());
+		});
+		window.heightProperty().addListener(e->{
+			if(window.getHeight()<window.getMinHeight()) window.setHeight(window.getMinHeight());
+		});
+	}
+	
+	private void loadImg(String img, Consumer<ImageView> onload){
+		Paintless.LOADING_POOL.execute(()->Platform.runLater(()->onload.accept(new ImageView(new Image(src(img))))));
+	}
+	
+	private void updateBorder(BorderPane borderPane0){
+		if(window.isMaximized()) borderPane0.setBorder(null);
+		else if(window.isFocused()) borderPane0.setBorder(new Border(new BorderStroke(winActiveBord, BorderStrokeStyle.SOLID, new CornerRadii(0), BorderWidths.DEFAULT)));
+		else borderPane0.setBorder(new Border(new BorderStroke(winUnactiveBord, BorderStrokeStyle.SOLID, new CornerRadii(0), BorderWidths.DEFAULT)));
+	}
+	
+	private InputStream src(String path){
+		return getClass().getResourceAsStream(path);
+	}
+	
+	private Button btn(String txt, EventHandler<ActionEvent> action, String...cls){
+		return btn(new Button(txt), action, cls);
+	}
+	
+	private Button btn(Button t, EventHandler<ActionEvent> action, String...cls){
+		t.setOnAction(action);
+		return cls(t, cls);
+	}
+	
+	private <T extends Node> T cls(T t, String...cls){
+		t.getStyleClass().addAll(cls);
+		return t;
+	}
+	
+	public void exitApp0(){
+		System.exit(0);
+	}
 }
